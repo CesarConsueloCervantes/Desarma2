e1214a6e86c149ebc82901563fdf162c8048dea8
cesario548(up230017) <up230017@alumnos.upa.edu.mx>
2025-06-16 16:54:39 -0600

    feat (indexRoutes.js) :sparkles: : se agreagon las rutas del usuario

--- DE2-API/src/entities/indexRoutes.js
+++ DE2-API/src/entities/indexRoutes.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const proveedorRoutes = require('./C_Proveedor/C_ProveedorRoutes');
+const usuarioRoutes = require('./T_Usuario/T_UsuarioRoutes')
 
 router.use(proveedorRoutes);
+router.use(usuarioRoutes)
 
 module.exports = router;
