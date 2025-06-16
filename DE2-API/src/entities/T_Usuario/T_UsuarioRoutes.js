137b637a8a5accf76ff3d624de62cab51f98b863
cesario548(up230017) <up230017@alumnos.upa.edu.mx>
2025-06-16 16:54:15 -0600

    feat (T_Usuarios) :sparkles: : se agregaron las rutas de los usuarios

---
+++ DE2-API/src/entities/T_Usuario/T_UsuarioRoutes.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const { body, param, query } = require('express-validator');
+const validate = require('../../middlewares/validation');
+const usuarioController = require('./T_UsuarioController');
+const authMiddleware = require('../../middlewares/authMiddlewares');
+
+const router = express.Router();
+
+//este es la ruta para obetener todos los Usuarios
+router.get('/usuario', [
+  query('page').optional().isInt({ min: 1 }).withMessage('Page must be a positive integer'),
+  query('limit').optional().isInt({ min: 1 }).withMessage('Limit must be a positive integer'),
+], validate, usuarioController.getUsuarios);
+
+//este es la ruta para obeter un Usuario
+router.get('/usuario/:id', [
+  param('id').isMongoId().withMessage('Invalid Usuario ID'),
+], validate, usuarioController.getUsuarioPorID);
+
+
+//este es la ruta para crear un usuario
+router.post('/usuario', [
+    check('T_Usuario_Password')
+    .exists({ checkFalsy: true }).withMessage('La contraseña es obligatoria')
+    .isLength({ max: 20, min: 8 }).withMessage('La contraseña no debe exceder los 20 caracteres'),
+
+  check('T_Usuario_Nombre')
+    .exists({ checkFalsy: true }).withMessage('El nombre es obligatorio')
+    .isLength({ max: 30 }).withMessage('El nombre no debe exceder los 30 caracteres'),
+
+  check('T_Usuario_Apellido')
+    .exists({ checkFalsy: true }).withMessage('El apellido es obligatorio')
+    .isLength({ max: 30 }).withMessage('El apellido no debe exceder los 30 caracteres'),
+
+  check('C_Proveedor_Telefono')
+    .exists({ checkFalsy: true }).withMessage('El teléfono es obligatorio')
+    .matches(/^[0-9]{10}$/).withMessage('El número de teléfono debe tener exactamente 10 dígitos'),
+
+  check('T_Usuario_Email')
+    .exists({ checkFalsy: true }).withMessage('El email es obligatorio')
+    .isEmail().withMessage('Debe ser un email válido'),
+
+  check('T_Usuario_Rol')
+    .exists({ checkFalsy: true }).withMessage('El rol es obligatorio')
+    .isIn(['cliente', 'administrador']).withMessage('Rol no válido'),
+
+  check('T_Usuario_Estado')
+    .exists({ checkFalsy: true }).withMessage('El estado es obligatorio')
+    .isBoolean().withMessage('El estado debe ser booleano'),
+
+  // Campos opcionales con validaciones
+  check('T_Usuario_Direccion_Calle')
+    .optional()
+    .isLength({ max: 30 }).withMessage('La calle no debe exceder los 30 caracteres'),
+
+  check('T_Usuario_Direccion_Fraccionamiento')
+    .optional()
+    .isLength({ max: 40 }).withMessage('El fraccionamiento no debe exceder los 40 caracteres'),
+
+  check('T_Usuario_Direccion_CP')
+    .optional()
+    .isLength({ max: 6 }).withMessage('El código postal no debe exceder los 6 caracteres'),
+
+  check('T_Usuario_Direccion_Ciudad')
+    .optional()
+    .isLength({ max: 20 }).withMessage('La ciudad no debe exceder los 20 caracteres'),
+
+  check('T_Usuario_Direccion_ProvinciaEstado')
+    .optional()
+    .isMongoId().withMessage('Provincia/Estado debe ser un ID válido'),
+
+  check('T_Usuario_Direccion_Pais')
+    .optional()
+    .isMongoId().withMessage('El país debe ser un ID válido')
+],validate, usuarioController.postUsuario)
+
+
+//este es la ruta para actualizar un usuario
+router.put('/usuario/:id', [
+    param('id').isMongoId().withMessage('Invalid Usuario ID'),
+    check('T_Usuario_Password')
+    .exists({ checkFalsy: true }).withMessage('La contraseña es obligatoria')
+    .isLength({ max: 20 }).withMessage('La contraseña no debe exceder los 20 caracteres'),
+
+  check('T_Usuario_Nombre')
+    .exists({ checkFalsy: true }).withMessage('El nombre es obligatorio')
+    .isLength({ max: 30 }).withMessage('El nombre no debe exceder los 30 caracteres'),
+
+  check('T_Usuario_Apellido')
+    .exists({ checkFalsy: true }).withMessage('El apellido es obligatorio')
+    .isLength({ max: 30 }).withMessage('El apellido no debe exceder los 30 caracteres'),
+
+  check('C_Proveedor_Telefono')
+    .exists({ checkFalsy: true }).withMessage('El teléfono es obligatorio')
+    .matches(/^[0-9]{10}$/).withMessage('El número de teléfono debe tener exactamente 10 dígitos'),
+
+  check('T_Usuario_Email')
+    .exists({ checkFalsy: true }).withMessage('El email es obligatorio')
+    .isEmail().withMessage('Debe ser un email válido'),
+
+  check('T_Usuario_Rol')
+    .exists({ checkFalsy: true }).withMessage('El rol es obligatorio')
+    .isIn(['cliente', 'administrador']).withMessage('Rol no válido'),
+
+  check('T_Usuario_Estado')
+    .exists({ checkFalsy: true }).withMessage('El estado es obligatorio')
+    .isBoolean().withMessage('El estado debe ser booleano'),
+
+  // Campos opcionales con validaciones
+  check('T_Usuario_Direccion_Calle')
+    .optional()
+    .isLength({ max: 30 }).withMessage('La calle no debe exceder los 30 caracteres'),
+
+  check('T_Usuario_Direccion_Fraccionamiento')
+    .optional()
+    .isLength({ max: 40 }).withMessage('El fraccionamiento no debe exceder los 40 caracteres'),
+
+  check('T_Usuario_Direccion_CP')
+    .optional()
+    .isLength({ max: 6 }).withMessage('El código postal no debe exceder los 6 caracteres'),
+
+  check('T_Usuario_Direccion_Ciudad')
+    .optional()
+    .isLength({ max: 20 }).withMessage('La ciudad no debe exceder los 20 caracteres'),
+
+  check('T_Usuario_Direccion_ProvinciaEstado')
+    .optional()
+    .isMongoId().withMessage('Provincia/Estado debe ser un ID válido'),
+
+  check('T_Usuario_Direccion_Pais')
+    .optional()
+    .isMongoId().withMessage('El país debe ser un ID válido')
+],validate, usuarioController.putUsuario)
+
+
+//este es la ruta para borrar un Usuario
+router.delete('/usuario/:id', [
+  param('id').isMongoId().withMessage('Invalid Usuario ID'),
+], validate, usuarioController.deleteUsuario);
+
+module.exports = router;
